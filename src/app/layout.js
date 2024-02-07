6f5ff975faafb95458963f583e1fd677cd976fbb
NOGNOG <149191240+nognog-dev@users.noreply.github.com>
2024-02-07 16:14:40 -0300

    fix(styles): resolving some bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ import "./global.scss";
 import toast, { Toaster } from "react-hot-toast";
 
 import { Rubik } from "next/font/google";
+import Head from "next/head";
 const rubik = Rubik({
   subsets: ["latin"],
   display: "swap"
@@ -33,6 +34,21 @@ export default function RootLayout({ children }) {
       }}
     >
       <html lang="en" className={rubik.className}>
+        <Head>
+          <title>NODFF: PDFs Merge & Compress Management</title>
+          <meta
+            name="description"
+            content="This is a powerful and user-friendly PDF management tool built with Next.js. It provides users with the ability to merge and compress PDF files with ease."
+          />
+          <meta
+            name="keywords"
+            content="pdf, design, development, pdf-document, pdf-merger, nognog, NOGNOG, Design, pdf-tool, compress pdf, compress, merge pdf, merge"
+          />
+          <meta name="author" content="nognog-dev" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="date" content="2024-02-06" />
+          <meta name="url" content="https://nodff-pdf.vercel.app/" />
+        </Head>
         <body style={{ margin: "unset" }}>
           <Header />
           {children}
